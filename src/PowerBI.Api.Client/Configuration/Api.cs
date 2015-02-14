7abc367ecaf8a0c140031ccd6b43e3f3f5872080
pontoreau <pontoreau.sylvain@gmail.com>
2015-02-14 17:44:40 +0100

    Add compilation condition for PCL

--- src/PowerBI.Api.Client/Configuration/Api.cs
+++ src/PowerBI.Api.Client/Configuration/Api.cs
@@ -5,15 +5,25 @@ namespace PowerBI.Api.Client.Configuration
 	/// <summary>
 	/// Api configuration element
 	/// </summary>
+	#if !PCL
 	public sealed class Api : ConfigurationElement
+	#else
+	public sealed class Api
+	#endif
 	{
 		/// <summary>
 		/// PowerBI Api url
 		/// </summary>
+		#if !PCL
 		[ConfigurationProperty("Url", IsRequired = true)]
+		#endif
 		public string Url
 		{
+			#if !PCL
 			get { return (string)this["Url"]; }
+			#else
+			get; set;
+			#endif
 		}
 	}
 }
